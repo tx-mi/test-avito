dc246b348b299c3901312c69a60db9980a661e97
Ramazan <txmiwxxd@gmail.com>
2022-11-05 15:49:09 +0300

    setup project
    create prjoect, Delete storyboards, create empty viewController

---
+++ Employees/Employees/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employees
+//
+//  Created by Ramazan Abdulaev on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
